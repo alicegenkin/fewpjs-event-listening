3a1c47eb10f98c1181e26d055028aab98b1aa9fe
Alice Genkin <agenkin1@gmail.com>
2021-05-12 17:52:15 -0700

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+//add event listener takes two arguments
+//the name of the event
+//a callback function to handle the event
+
 function addingEventListener() {
+  const input = document.getElementById('input');
+input.addEventListener('click', function(event) {
+  alert('I was clicked!');
+}); 
 }
